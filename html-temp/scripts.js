a4574da478550ae53caccfac1b6119a09f9bb628
ASUS <Yehor Kholodiuk>
2022-04-23 13:32:47 +0300

    initial commit

--- html-temp/scripts.js
+++ html-temp/scripts.js
@@ -1,13 +1,13 @@
 console.log('---JS---')
 
 const titles = document.getElementsByTagName('h2')
-
+const  idOne = document.getElementById('one')
+console.log(idOne.innerText)
 for (let i = 0; i < titles.length; i++) {
  console.log(titles[i])
 }
 let i = 5;
-const  isOne = document.getElementById('one')
-console.log(isOne)
+
 function plusOne() {
     console.log(++i)
 };
